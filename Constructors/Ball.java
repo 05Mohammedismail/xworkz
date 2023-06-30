eb88f45b281d7f69579802d598b1f248dfb00083
05Mohammed Ismail <ismail.xworkz@gmail.com>
2023-06-30 08:58:57 +0530

    push

--- Constructors/Ball.java
+++ Constructors/Ball.java
@@ -1,6 +1,6 @@
 class Ball{
-	public Ball(int value){
-			System.out.println("The number of Ball is " + value);
+	public Ball(int number){
+			System.out.println("The number of Ball is " + number);
 	}
 }
 
