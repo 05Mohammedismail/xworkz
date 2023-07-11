c1f2ff3d2c029f7c40e493c1668e1e5d2be66a89
05Mohammed Ismail <ismail.xworkz@gmail.com>
2023-07-11 23:40:08 +0530

    My first Eclipse Programme

---
+++ FirstOne/src/com/Manoj.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Manoj {
+
+	public static void main(String[] args) {
+		for(int value = 0; value < 100 ; ) {
+			     value++;
+		System.out.println("Manoj had a breakUP" +"-->"+ value + "times");
+		}
+
+	}
+
+}
