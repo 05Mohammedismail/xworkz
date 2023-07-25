4ad44a333100d6a38682651ed718e1664f94f590
05Mohammed Ismail <ismail.xworkz@gmail.com>
2023-07-25 21:43:43 +0530

    Object Class is the Super class of all Classes

---
+++ July25_SuperClass/src/com/xworkz/Prime/Runner/AllRunner.java
@@ -0,0 +1,91 @@
+package com.xworkz.Prime.Runner;
+
+import com.xworkz.Prime.app.*;
+
+public class AllRunner {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Running main method in ALl Runner\n");
+		 System.out.println("* * * * * * * * * * * * * * * * * * * *");
+		//Finance extends Object
+			Object obj1 = new Finance();
+			obj1.toString();
+		
+			Finance finance = new Finance();
+			finance.toString();
+			 System.out.println("* * * * * * * * * * * * * * * * * * * *");
+		
+		//Ladoo extends Object
+			Object obj2 = new Ladoo();
+			obj2.toString();
+				
+			Ladoo ladoo = new Ladoo();
+			ladoo.toString();
+			 System.out.println("* * * * * * * * * * * * * * * * * * * *");
+						
+		//Lake extends Object
+				Object obj3 = new Lake();
+				obj3.toString();
+				
+				Lake lake = new Lake();
+				lake.toString();
+				 System.out.println("* * * * * * * * * * * * * * * * * * * *");
+				
+		//Lotus extends Object
+				Object obj4 = new Lotus();
+				obj4.toString();
+				
+				Lotus lotus = new Lotus();
+				lotus.toString();
+				 System.out.println("* * * * * * * * * * * * * * * * * * * *");
+				
+		//MLA extends Object
+				Object obj5 = new MLA();
+				obj5.toString();
+				
+				MLA mla = new MLA();
+				mla.toString();
+				 System.out.println("* * * * * * * * * * * * * * * * * * * *");
+				
+		//Money extends Object
+				Object obj6 = new Money();
+				obj6.toString();
+				
+				Money money = new Money();
+				money.toString();
+				 System.out.println("* * * * * * * * * * * * * * * * * * * *");
+				
+		//President extends Object
+				Object obj7 = new President();
+				obj7.toString();
+				
+				President president = new President();
+				president.toString();
+				 System.out.println("* * * * * * * * * * * * * * * * * * * *");
+				
+		//PrimeMinister extends Object
+				Object obj8 = new PrimeMinister();
+				obj8.toString();
+				
+				PrimeMinister pm = new PrimeMinister();
+				pm.toString();
+				 System.out.println("* * * * * * * * * * * * * * * * * * * *");
+				
+		//River extends Object
+				Object obj9 = new River();
+				obj9.toString();
+				
+				River river = new River();
+				river.toString();
+				 System.out.println("* * * * * * * * * * * * * * * * * * * *");
+				
+		//Security extends Object
+				Object obj10 = new Security();
+				obj10.toString();
+				
+				Security security = new Security();
+				security.toString();
+	}
+
+}
